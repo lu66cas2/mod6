43744f6afffed1180b135daa8aabe4fbe68c01c1
lu66cas2 <lu66cas.macedo@gmail.com>
2023-07-15 01:44:15 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Algoritimo acordar:\n" +
+                "\n" +
+                "Abrir o olho\n" +
+                "remover o cobertor\n" +
+                "Levantar da cama\n" +
+                "verificar a hora\n" +
+                "verificar o celular\n" +
+                "caminhar até a porta do quarto\n" +
+                "abrir a porta do quarto\n" +
+                "caminhar ao banheiro\n" +
+                "abrir a porta do banheiro\n" +
+                "usar o banheiro\n" +
+                "caminhar a cozinha\n" +
+                "abrir a geladeira\n" +
+                "pegar um lanche\n" +
+                "sentar a mesa\n" +
+                "comer o lanche\n" +
+                "\n" +
+                "\n");
+    }
+}
